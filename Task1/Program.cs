9f44daaf218e7dc638d9196e5be49bd46f4269a8
Rina Antropova <rin.antropova@gmail.com>
2024-05-24 14:52:52 +0300

    Task1 is done - values from M to N using increment and recursion

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void PrintNaturalNum(int a, int b)
+{
+
+    Console.Write($"{a} ");
+    if (a == b) return;
+    int increment = (b - a) / Math.Abs(b - a);
+    PrintNaturalNum(a + increment, b);
+
+
+}
+
+
+
+Console.WriteLine("Input number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNaturalNum(m, n);
